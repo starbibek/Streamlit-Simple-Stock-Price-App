724e57ed4172a84172094f0a5e0f4970b75afd83
Shobhit Paudel <starbibek2@gmail.com>
2024-12-30 12:42:42 +0545

    first Comment

---
+++ myapp.py
@@ -0,0 +1,34 @@
+import yfinance as yf
+import pandas as pd
+import streamlit as st
+import requests
+
+st.write(
+    """
+         # Simple Stock Price App
+         
+         Get the stock closing price and volume from input!
+         
+         """
+)
+
+chooseBy = st.radio(label="Type", options=["Company", "Ticker Symbol"])
+
+search = st.text_input(f"{chooseBy.title()}")
+tickerSymbol = ""
+
+# Checking for type choosen and handling
+if chooseBy.title() == "Company" and search != "":
+    fetch = requests.get(f"https://stock-symbol-lookup-api.onrender.com/{search}")
+    tickerSymbol = fetch.json().get("stock_symbol", "")
+else:
+    tickerSymbol = search
+
+
+if tickerSymbol != "":
+    tickerData = yf.Ticker(tickerSymbol)
+    tickerDf = tickerData.history(period="1d", start="2010-5-31", end="2020-5-31")
+    st.write("Closing Prices")
+    st.line_chart(tickerDf.Close)
+    st.write("Values Prices")
+    st.line_chart(tickerDf.Volume)
